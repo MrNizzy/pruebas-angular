45aa9e3733af76c4450a345450934b5232d507c2
Nizzy Hernandez <3134183631y@gmail.com>
2022-06-10 20:19:56 -0500

    Add bootstrap and new views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
+/* Importing the components */
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 
-// app routes angular constant
+/* app routes angular constant */ 
 const appRoutes: Routes = [
   { path: 'logout', component: LogoutComponent },
   { path: 'login', component: LoginComponent },
